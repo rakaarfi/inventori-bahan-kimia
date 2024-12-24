d1228506c30f1ba9c8fb6c10ac8d41cadb5be56e
rakaarfi <rakaarfi@gmail.com>
2024-12-24 22:30:51 +0700

    small changes

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ from routes.data_pabrik_pembuat import router as data_pabrik_pembuat_router
 from routes.lokasi_bahan_kimia import router as lokasi_bahan_kimia_router
 from routes.data_bahan_kimia import router as data_bahan_kimia_router
 from routes.data_penerimaan_penggunaan import router as data_penerimaan_penggunaan_router
-
+from routes.reports import router as reports_router
 
 app = FastAPI()
 
-origins = [
-    "*"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -34,12 +32,32 @@ def on_startup():
     create_db_and_tables()
 
 # Menambahkan router ke aplikasi FastAPI
-app.include_router(index_router, prefix="", tags=["Index"])
-
-app.include_router(lokasi_bahan_kimia_router, prefix="/lokasi_bahan_kimia", tags=["Lokasi Bahan Kimia"])
-
-app.include_router(data_pabrik_pembuat_router, prefix="/data_pabrik_pembuat", tags=["Data Pabrik Pembuat"])
-
-app.include_router(data_bahan_kimia_router, prefix="/data_bahan_kimia", tags=["Data Bahan Kimia"])
-
-app.include_router(data_penerimaan_penggunaan_router, prefix="/data_penerimaan_penggunaan", tags=["Penerimaan Penggunaan"])
+app.include_router(
+    index_router, 
+    prefix="", 
+    tags=["Index"]
+    )
+app.include_router(
+    lokasi_bahan_kimia_router, 
+    prefix="/lokasi_bahan_kimia", 
+    tags=["Lokasi Bahan Kimia"]
+    )
+app.include_router(
+    data_pabrik_pembuat_router, 
+    prefix="/data_pabrik_pembuat", 
+    tags=["Data Pabrik Pembuat"]
+    )
+app.include_router(
+    data_bahan_kimia_router, 
+    prefix="/data_bahan_kimia", 
+    tags=["Data Bahan Kimia"]
+    )
+app.include_router(
+    data_penerimaan_penggunaan_router, 
+    prefix="/data_penerimaan_penggunaan", 
+    tags=["Penerimaan Penggunaan"])
+app.include_router(
+    reports_router, 
+    prefix="/reports", 
+    tags=["Reports"]
+    )
